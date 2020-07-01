ad73d859558ea16c48da2b173ab180b4f0f542b6
Armando <armandovanoeffelen@gmail.com>
2020-07-01 15:22:16 +0200

    made an error in the gitignore

--- resources/views/profile/template/index.blade.php
+++ resources/views/profile/template/index.blade.php
@@ -20,8 +20,9 @@
 <!-- Page Content -->
 <div class="content content-full content-boxed">
     <!-- Latest Friends -->
+
     <h2 class="content-heading">
-        <i class="si si-directions mr-1"></i> Recent bekeken workshops
+        <i class="si si-check mr-1"></i> Afgeronde workshops
     </h2>
     <div class="row">
         <div class="col-md-6 col-xl-3">
@@ -36,9 +37,9 @@
                     <a class="btn btn-sm btn-light" href="javascript:void(0)">
                         <i class="fa fa-search text-muted mr-1"></i> Bekijk
                     </a>
-                    <a class="btn btn-sm btn-light" href="javascript:void(0)">
+                    <button class="btn btn-sm btn-light" href="javascript:void(0)">
                         <i class="fa fa-star text-warning mr-1"></i> Favoriet
-                    </a>
+                    </button>
                 </div>
             </div>
         </div>
@@ -54,45 +55,9 @@
                     <a class="btn btn-sm btn-light" href="javascript:void(0)">
                         <i class="fa fa-search text-muted mr-1"></i> Bekijk
                     </a>
-                    <a class="btn btn-sm btn-light" href="javascript:void(0)">
+                    <button class="btn btn-sm btn-light" href="javascript:void(0)">
                         <i class="fa fa-star text-warning mr-1"></i> Favoriet
-                    </a>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-6 col-xl-3">
-            <div class="block block-rounded text-center">
-                <div class="block-content block-content-full bg-image"
-                     style="background-image: url('https://www.bravissamenvitaal.nl/wp-content/uploads/2020/02/iStock-1058457940-2-495x400.jpg'); height: 150px;">
-                </div>
-                <div class="block-content block-content-full block-content-sm bg-body-light">
-                    <div class="font-w600">Workshop titel</div>
-                </div>
-                <div class="block-content block-content-full">
-                    <a class="btn btn-sm btn-light" href="javascript:void(0)">
-                        <i class="fa fa-search text-muted mr-1"></i> Bekijk
-                    </a>
-                    <a class="btn btn-sm btn-light" href="javascript:void(0)">
-                        <i class="fa fa-star text-warning mr-1"></i> Favoriet
-                    </a>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-6 col-xl-3">
-            <div class="block block-rounded text-center">
-                <div class="block-content block-content-full bg-image"
-                     style="background-image: url('https://www.bravissamenvitaal.nl/wp-content/uploads/2020/02/iStock-1058457940-2-495x400.jpg'); height: 150px;">
-                </div>
-                <div class="block-content block-content-full block-content-sm bg-body-light">
-                    <div class="font-w600">Workshop titel</div>
-                </div>
-                <div class="block-content block-content-full">
-                    <a class="btn btn-sm btn-light" href="javascript:void(0)">
-                        <i class="fa fa-search text-muted mr-1"></i> Bekijk
-                    </a>
-                    <a class="btn btn-sm btn-light" href="javascript:void(0)">
-                        <i class="fa fa-star text-warning mr-1"></i> Favoriet
-                    </a>
+                    </button>
                 </div>
             </div>
         </div>
@@ -170,6 +135,83 @@
                     <button class="btn btn-sm btn-light" href="javascript:void(0)" disabled>
                         <i class="fa fa-star text-warning mr-1"></i> Favoriet
                     </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <h2 class="content-heading">
+        <i class="si si-directions mr-1"></i> Recent bekeken workshops
+    </h2>
+    <div class="row">
+        <div class="col-md-6 col-xl-3">
+            <div class="block block-rounded text-center">
+                <div class="block-content block-content-full bg-image"
+                     style="background-image: url('https://www.bravissamenvitaal.nl/wp-content/uploads/2020/02/iStock-1058457940-2-495x400.jpg'); height: 150px;">
+                </div>
+                <div class="block-content block-content-full block-content-sm bg-body-light">
+                    <div class="font-w600">Workshop titel</div>
+                </div>
+                <div class="block-content block-content-full">
+                    <a class="btn btn-sm btn-light" href="javascript:void(0)">
+                        <i class="fa fa-search text-muted mr-1"></i> Bekijk
+                    </a>
+                    <a class="btn btn-sm btn-light" href="javascript:void(0)">
+                        <i class="fa fa-star text-warning mr-1"></i> Favoriet
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-xl-3">
+            <div class="block block-rounded text-center">
+                <div class="block-content block-content-full bg-image"
+                     style="background-image: url('https://www.bravissamenvitaal.nl/wp-content/uploads/2020/02/iStock-1058457940-2-495x400.jpg'); height: 150px;">
+                </div>
+                <div class="block-content block-content-full block-content-sm bg-body-light">
+                    <div class="font-w600">Workshop titel</div>
+                </div>
+                <div class="block-content block-content-full">
+                    <a class="btn btn-sm btn-light" href="javascript:void(0)">
+                        <i class="fa fa-search text-muted mr-1"></i> Bekijk
+                    </a>
+                    <a class="btn btn-sm btn-light" href="javascript:void(0)">
+                        <i class="fa fa-star text-warning mr-1"></i> Favoriet
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-xl-3">
+            <div class="block block-rounded text-center">
+                <div class="block-content block-content-full bg-image"
+                     style="background-image: url('https://www.bravissamenvitaal.nl/wp-content/uploads/2020/02/iStock-1058457940-2-495x400.jpg'); height: 150px;">
+                </div>
+                <div class="block-content block-content-full block-content-sm bg-body-light">
+                    <div class="font-w600">Workshop titel</div>
+                </div>
+                <div class="block-content block-content-full">
+                    <a class="btn btn-sm btn-light" href="javascript:void(0)">
+                        <i class="fa fa-search text-muted mr-1"></i> Bekijk
+                    </a>
+                    <a class="btn btn-sm btn-light" href="javascript:void(0)">
+                        <i class="fa fa-star text-warning mr-1"></i> Favoriet
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-xl-3">
+            <div class="block block-rounded text-center">
+                <div class="block-content block-content-full bg-image"
+                     style="background-image: url('https://www.bravissamenvitaal.nl/wp-content/uploads/2020/02/iStock-1058457940-2-495x400.jpg'); height: 150px;">
+                </div>
+                <div class="block-content block-content-full block-content-sm bg-body-light">
+                    <div class="font-w600">Workshop titel</div>
+                </div>
+                <div class="block-content block-content-full">
+                    <a class="btn btn-sm btn-light" href="javascript:void(0)">
+                        <i class="fa fa-search text-muted mr-1"></i> Bekijk
+                    </a>
+                    <a class="btn btn-sm btn-light" href="javascript:void(0)">
+                        <i class="fa fa-star text-warning mr-1"></i> Favoriet
+                    </a>
                 </div>
             </div>
         </div>
