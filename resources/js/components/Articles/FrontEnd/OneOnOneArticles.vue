81eaac6443528b915f0756ffc752e2224a435da2
Armando <armandovanoeffelen@gmail.com>
2020-12-23 22:19:21 +0100

    testing

--- resources/js/components/Articles/FrontEnd/OneOnOneArticles.vue
+++ resources/js/components/Articles/FrontEnd/OneOnOneArticles.vue
@@ -11,7 +11,7 @@
                         <a v-if="categories[0].article[0].has_video === 0"
                            class="block block-transparent w-100 d-md-flex align-items-md-stretch bg-image"
                            v-bind:style="[categories[0].article[0].image_link ?
-                                    {'background': 'url(' + categories[0].article[0].image_link + ')'} :
+                                    {'background': 'url(../' + categories[0].article[0].image_link + ')'} :
                                      {'background-image': 'url('+ default_image +')'}]"
                            v-bind:href="'/article/'+categories[0].article[0].id +'/show'"
                            data-toggle="click-ripple">
