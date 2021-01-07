a2085d1d528a91a31fed26eae6b324d24d782073
Dennis <dennis.krijgsman@hotmail.com>
2021-01-07 21:57:01 +0100

    changed name calender

--- resources/views/activiteitenkalender/index.blade.php
+++ resources/views/activiteitenkalender/index.blade.php
@@ -3,11 +3,11 @@
     <div class="bg-body-light">
         <div class="content content-full">
             <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center">
-                <h1 class="flex-sm-fill font-size-h2 font-w400 mt-2 mb-0 mb-sm-2">Activiteitenkalender</h1>
+                <h1 class="flex-sm-fill font-size-h2 font-w400 mt-2 mb-0 mb-sm-2">Kalender Gelre Energiek</h1>
                 <nav class="flex-sm-00-auto ml-sm-3" aria-label="breadcrumb">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item">GelreEnergiek</li>
-                        <li class="breadcrumb-item active" aria-current="page">Activiteitenkalender</li>
+                        <li class="breadcrumb-item active" aria-current="page">Kalender Gelre Energiek</li>
                     </ol>
                 </nav>
             </div>
@@ -19,7 +19,7 @@
                 <a href="javascript:history.back();" class="btn btn-primary">Terug</a>
             </div>
             <div class="col-md-12 col-lg-8 col-xl-8 col-12 col-sm-12 col-xs-12">
-                <h3>Activiteitenkalender</h3>
+                <h3>Kalender Gelre Energiek</h3>
             </div>
             <div class="col-md-12 col-lg-2 col-xl-2 col-12 col-sm-12 col-xs-12 cols-12">
             </div>
