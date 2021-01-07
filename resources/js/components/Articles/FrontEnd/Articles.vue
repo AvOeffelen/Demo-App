9c9ad3cce76c777cf223f866874e4d92f13f9fcf
Armando <armandovanoeffelen@gmail.com>
2021-01-07 21:22:55 +0100

    this shouldnt be one.

--- resources/js/components/Articles/FrontEnd/Articles.vue
+++ resources/js/components/Articles/FrontEnd/Articles.vue
@@ -24,7 +24,7 @@
                                 <a v-if="category.article[0].has_video === false"
                                    class="block block-transparent w-100 d-md-flex align-items-md-stretch bg-image h-286"
                                    v-bind:style="{
-                                        'background':'url(/' + category.article[1].image_link + ')',
+                                        'background':'url(/' + category.article[0].image_link + ')',
                                         'background-position': 'center',
                                         'background-size': 'cover',
                                         'background-repeat': 'no-repeat',
