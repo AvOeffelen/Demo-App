c1b793f7d7e3aec84b6f9dc8e0956c569481accd
Dennis <dennis.krijgsman@hotmail.com>
2021-01-11 21:55:39 +0100

    text changes

--- resources/js/components/Dashboard/DashboardWithoutRibbon.vue
+++ resources/js/components/Dashboard/DashboardWithoutRibbon.vue
@@ -24,7 +24,7 @@
                             Activiteitenkalender.
                         </div> -->
                         <div class="pt-4 pb-6 px-md-3">
-                            <h3 class="h1 font-w700 text-white mb-1">Activiteitenkalender</h3>
+                            <h3 class="h1 font-w700 text-white mb-1">Ons Aanbod</h3>
                         </div>
                     </div>
                 </a>
--- resources/views/activiteitenkalender/index.blade.php
+++ resources/views/activiteitenkalender/index.blade.php
@@ -3,23 +3,23 @@
     <div class="bg-body-light">
         <div class="content content-full">
             <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center">
-                <h1 class="flex-sm-fill font-size-h2 font-w400 mt-2 mb-0 mb-sm-2">Activiteitenkalender</h1>
+                <h1 class="flex-sm-fill font-size-h2 font-w400 mt-2 mb-0 mb-sm-2">Ons Aanbod</h1>
                 <nav class="flex-sm-00-auto ml-sm-3" aria-label="breadcrumb">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item">KLANTSAMENVITAAL</li>
-                        <li class="breadcrumb-item active" aria-current="page">Activiteitenkalender</li>
+                        <li class="breadcrumb-item active" aria-current="page">Ons Aanbod</li>
                     </ol>
                 </nav>
             </div>
         </div>
     </div>
-    <div class="content content-full text-center">
+    <div class="content content-full ">
         <div class="row">
             <div class="col-md-12 col-lg-2 col-xl-2 col-12 col-sm-12 col-xs-12 cols-12">
                 <a href="javascript:history.back()" class="btn btn-primary">Terug</a>
             </div>
             <div class="col-md-12 col-lg-8 col-xl-8 col-12 col-sm-12 col-xs-12">
-                <h3>Activiteitenkalender</h3>
+                <h3>Ons Aanbod</h3>
             </div>
             <div class="col-md-12 col-lg-2 col-xl-2 col-12 col-sm-12 col-xs-12 cols-12">
             </div>
@@ -28,7 +28,41 @@
             <div class="col-md-12 col-lg-2 col-xl-2 col-12 col-sm-12 col-xs-12 cols-12">
             </div>
             <div class="col-md-12 col-lg-8 col-xl-8 col-12 col-sm-12 col-xs-12">
-                <iframe src="{{URL::to('/storage/pdf/activiteitenkalender.pdf')}}" width="100%" height="800"></iframe>
+            <alinea>
+                <p><b>Betrokken medewerkers</b> staan aan de basis van productiviteit. Wanneer medewerkers minder betrokken zijn bij het werk en de organisatie, zal dit een impact hebben op de productiviteit. </p>
+                <p><b>Fitte medewerkers</b> kunnen hun werk beter aan.
+    Ze hebben meer energie, zijn creatiever en socialer.
+    Hoe meer fitte medewerkers uw organisatie telt, hoe lager het verzuim. En dat scheelt een hoop geld, gedoe en energie. Als werkgever wil je daarvoor interventies, begeleiding en dienstverlening op maat beschikbaar stellen om medewerkers daadwerkelijk fit te houden voor nu en in de toekomst.</p>
+                <strong>Dit platform biedt de oplossing!</strong>
+                <p>Hoe maakt u op een plek duidelijk waar een medewerker gebruik van kan maken?
+Één plek, laagdrempelig, toepasbaar en met linken naar veilige omgevingen waar er data wordt opgeslagen.</p>
+                <strong>GAMIFICATION:</strong>
+                <p>Toepassing van games als manier van leren. Spelen is eigenlijk onze natuurlijke manier van leren maar vanaf het moment dat we naar school gaan, komt steeds meer de nadruk te liggen op boekenkennis. Door middel van gamification bieden wij een leerstrategie  om het gedrag van medewerkers op een duurzame manier positief te beïnvloeden.
+</p>
+                <strong>UW ACADEMY:</strong>
+                <p>De mogelijk bestaat om een leeromgeving met ruim 700 online trainingen, actuele webinars en persoonlijkheidstesten onderdeel te laten zijn van dit platform.</p>
+                <strong>VRAGENLIJST:</strong>
+                <p>Naast verbinding maken met Johan zijn wij ook in staat om Whappbots voor u te maken. Het is een speels intelligent systeem, een techniek dat gebaseerd is op speelse interactie. Vragenlijsten tot leven laten komen vanuit de intrinsieke motivaties van medewerkers. Het zorgt ervoor dat mensen willen doen wat jij nodig hebben door middel van toepassing van kunstmatige intelligentie.</p>
+                <a class="btn btn-primary" href="/vragenlijsten" role="button"  target="_blank">Ga naar Vragenlijsten</a>
+                <p></p>
+                <strong>UW AANBOD:</strong>
+                <p>Uw volledig aanbod op maat ingericht. Zie mogelijkheden van inrichting en weergave onder workshops of andere tegels.</p>
+                <a class="btn btn-primary" href="/workshop" role="button"  target="_blank">Ga naar workshops</a>
+                <p></p>
+                <strong>VORMGEVING:</strong>
+                <p>Uw platform wordt op maat voor u gemaakt. Met eigen logo, eigen kleuren en eigen vormgeving. Een aantal voorbeelden hebben wij voor u uitgewerkt. Klik op voorbeeld 1,2, of 3 voor een paar voorbeelden van variaties.</p>
+                <a class="btn btn-primary" href="/" role="button"  target="_blank">Bekijk voorbeelden</a>
+                <p></p>
+                <strong>INSTRUCTIEFILMS/INSPIRATIEMATERIAAL:</strong>
+                <p>Uw medewerkers inspireren met filmpjes, podcast, artikelen, het is allemaal mogelijk. Wij kunnen dit voor u plaatsen of u beschikt over eigen materiaal dat geplaatst kan worden. Een filmpje met een inspirerend verhaal van uw directeur, medewerkers aan het woord die graag iets willen delen of youtube filmpjes. Het kan allemaal geplaatst worden.
+</p>
+            </alinea>
+            <alinea>
+                <p>Dit platform biedt vele mogelijkheden. Enkele voorbeelden hebben wij voor u weergegeven maar wij kijken met u mee wat uw wensen zijn.
+Medewerkers kunnen een omgeving aanmaken waarin hun favorieten worden bijgehouden. Aansluitend kunnen wij ze attenderen op passend aanbod gezien de favorieten van de medewerker.
+Wanneer er data wordt opgeslagen zoals bij het invullen van vragenlijsten in Johan gaat de medewerker naar de andere omgeving, zorgt voor het aanmaken van een account in betreffende omgeving zodat alle privacy wordt gewaarborgd.
+De mogelijkheden om uw platform naar uw wensen in te richten, zijn onbeperkt. Dit alles vergroot de kans op gebruik en deelname van de medewerkers!</p>
+            </alinea>
             </div>
             <div class="col-md-12 col-lg-2 col-xl-2 col-12 col-sm-12 col-xs-12 cols-12">
             </div>
