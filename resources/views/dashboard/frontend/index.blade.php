d50c5f8d410206dd578ad576ff2dc374c73da687
Dennis <dennis.krijgsman@hotmail.com>
2021-01-14 14:35:33 +0100

    logo fix

--- resources/views/dashboard/frontend/index.blade.php
+++ resources/views/dashboard/frontend/index.blade.php
@@ -5,7 +5,7 @@
     <div class="bg-body-white">
         <div class="content content-full">
             <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center bravis-logo">
-                <img src="storage/logo/logo.jpg" height="70">
+                <img src="/storage/logo/logo.jpg" height="70">
                 <i class="far fa-lightbulb font-size-h4 font-w400"> Tip van de dag hier</i>
             </div>
         </div>
