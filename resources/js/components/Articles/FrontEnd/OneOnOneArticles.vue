62795ff2b64f18eea7a72b318dc6aad84e2824b4
Dennis <dennis.krijgsman@hotmail.com>
2021-01-15 13:52:14 +0100

    different image

--- resources/js/components/Articles/FrontEnd/OneOnOneArticles.vue
+++ resources/js/components/Articles/FrontEnd/OneOnOneArticles.vue
@@ -7,9 +7,10 @@
                         <b-button @click="goBack()" variant="primary">Terug</b-button>
                     </b-col>
                     <b-col cols="12" md="12" lg="6" xl="6" sm="12">
-                        <div class="text-center py-3 oneOnOneimage">
+                        <div class="text-center py-3">
                             <h1 class="h1 font-w700 mb-2 text-secondary text-shadow-workshops">Persoonlijke
                                 begeleiding</h1>
+                            <img src="storage/images/iconen nieuwe stijl Gelre.jpg" height="">
                         </div>
                     </b-col>
                     <b-col cols="12" md="12" lg="3" xl="3" sm="12">
