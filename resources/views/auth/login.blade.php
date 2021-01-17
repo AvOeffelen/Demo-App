c3f214d45ea5b2c8de15c72f7f8c41f4307e6f82
Dennis <dennis.krijgsman@hotmail.com>
2021-01-17 21:14:07 +0100

    put slogan

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -63,7 +63,7 @@ Wanneer biedt dit voor jou toegevoegde waarde? Dat is wanneer je het leuk vindt 
             <div class="hero-static col-md-6 d-none d-md-flex align-items-md-center justify-content-md-center text-md-center bg-gd-xksv">
                 <div class="p-3">
                     <p class="display-4 font-w700 text-xksv-lighter mb-3">
-                        Uw slogan hier!
+                        Zorg beter voor elkaar!
                     </p>
                     <p class="font-size-lg font-w600 text-xksv-lighter mb-0">
                         Copyright &copy; <span data-toggle="year-copy"></span>
