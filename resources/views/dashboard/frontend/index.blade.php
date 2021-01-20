0e9f6b78911992bdd1ebb0fd37112afc7d29a818
Dennis <dennis.krijgsman@hotmail.com>
2021-01-20 14:10:37 +0100

    text homepage

--- resources/views/dashboard/frontend/index.blade.php
+++ resources/views/dashboard/frontend/index.blade.php
@@ -7,6 +7,9 @@
             <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center bravis-logo">
                 <img src="storage/logo/logo.jpg" height="">
             </div>
+            <div class="text-center">
+                <p>Deze Gelre Energiek Gids is uitsluitend bestemd voor medewerkers van Gelre ziekenhuizen.</p>
+            </div>
         </div>
     </div>
     <div class="content content-boxed">
