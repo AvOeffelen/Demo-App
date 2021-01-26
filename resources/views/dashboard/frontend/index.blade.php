c2b76e71747023d6bd4c62a3f97e3d441eb5b741
Dennis <dennis.krijgsman@hotmail.com>
2021-01-26 16:56:13 +0100

    text slider

--- resources/views/dashboard/frontend/index.blade.php
+++ resources/views/dashboard/frontend/index.blade.php
@@ -6,6 +6,7 @@
         <div class="content content-full">
             <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center bravis-logo">
                 <img src="/storage/logo/logo.jpg" height="70">
+                <marquee width="60%" direction="left">Tip van de dag: Dit is een tip van de dag</marquee>
                 <a href="/verhalen">
                     <i class="fas fa-feather-alt "> Verhalen van medewerkers</i>
                 </a>
