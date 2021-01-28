059503e5bab4c2896ab58b4c7216140b9667ae84
Dennis <dennis.krijgsman@hotmail.com>
2021-01-28 16:45:34 +0100

    aanbod zorgverzekeraars

---
+++ resources/views/zorgverzekeraars/index.blade.php
@@ -0,0 +1,75 @@
+@extends('layouts.user')
+@section('content')
+    <div class="bg-body-light">
+        <div class="content content-full">
+            <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center">
+                <h1 class="flex-sm-fill font-size-h2 font-w400 mt-2 mb-0 mb-sm-2">Aanbod zorgverzekeraars</h1>
+                <nav class="flex-sm-00-auto ml-sm-3" aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item">KLANTSAMENVITAAL</li>
+                        <li class="breadcrumb-item active text-primary" aria-current="page">Aanbod zorgverzekeraars</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
+    <div class="content content text-center">
+    
+        <div class="row">
+            <div class="col-md-12 col-lg-2 col-xl-2 col-12 col-sm-12 col-xs-12 cols-12">
+                <a href="javascript:history.back()" class="btn btn-primary">Terug</a>
+            </div>
+            <div class="col-md-12 col-lg-8 col-xl-8 col-12 col-sm-12 col-xs-12">
+                
+                <h3>Aanbod zorgverzekeraars</h3>
+            </div>
+            <div class="col-md-12 col-lg-2 col-xl-2 col-12 col-sm-12 col-xs-12 cols-12">
+            </div>
+        </div>
+       
+        <div class="row">
+            <div class="col-md-12 col-lg-2 col-xl-2 col-12 col-sm-12 col-xs-12 cols-12">
+            </div>
+            <div class="col-md-12 col-lg-8 col-xl-8 col-12 col-sm-12 col-xs-12">
+                <alinea class="text-center">
+                    <p>Voor onlinetrainingen waar je nooit genoeg van krijgt kun je gaan naar de omgeving van
+                        GoodHabitz. Je vindt hier van toffe documentaires tot interessante magazines en van inspirerende
+                        experts tot uitdagende opdrachten. Je luistert, kijkt of doet allen de onderdelen die passen bij
+                        jouw manier van leren. Alles mag, niets moet: je leert zoals jij dat wilt!
+                        <br />
+                        <br />
+                        Je moet op deze pagina eerst even inloggen en wanneer je dat nog niet hebt gedaan, eerst een
+                        account aanmaken. Dit is omdat er persoonlijke gegevens van je worden opgeslagen en om die
+                        gegevens te beveiligen is een inlog noodzakelijk.
+                        <br />
+                        <br />
+                        <b>LET OP:</b>
+                        
+                        Wanneer je al eerder hebt ingelogd lukt het je nu waarschijnlijk niet om in te loggen. Klik op wachtwoord vergeten en je kunt weer gewoon bij het aanbod van GoodHabitz.
+
+                        Heb je nog niet eerder een account aangemaakt dan willen wij je verzoeken om een mail te sturen naar <a href="mailto:samenvitaal@bravis.nl" target="_blank">samenvitaal@bravis.nl </a>
+                        Van een medewerker ontvang je dan een link om een account voor jezelf aan te maken.
+
+                        Mocht bovenstaande onduidelijk zijn en/of vragen oproepen, stuur dan een mail naar <a href="mailto:samenvitaal@bravis.nl" target="_blank">samenvitaal@bravis.nl </a>
+
+                    </p>
+                </alinea>
+            </div>
+            <div class="col-md-12 col-lg-2 col-xl-2 col-12 col-sm-12 col-xs-12 cols-12">
+            </div>
+        </div>
+        <div class="row py-5">
+            <div class="col-md-4 col-lg-4 col-xl-4 col-4 col-sm-4 col-xs-4">
+            </div>
+            <div class="col-md-4 col-lg-4 col-xl-4 col-4 col-sm-4 col-xs-4">
+                <div class="text-center">
+                    <a href="https://my.goodhabitz.com/nl-be/login.html" target="_blank" class="btn btn-primary">Ga naar
+                        GoodHabitz</a>
+                </div>
+            </div>
+            <div class="col-md-4 col-lg-4 col-xl-4 col-4 col-sm-4 col-xs-4">
+            </div>
+            <img class="goodhabitz-image" src="storage/images/GOODHABITZ_Bravis_KlantSamenVitaal.nl_V1.png" height="400">
+        </div>
+    </div>
+@endsection
