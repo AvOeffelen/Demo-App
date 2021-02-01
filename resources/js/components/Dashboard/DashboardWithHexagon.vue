f6c0e02884f980df3eed39687ff2ca24893ab71a
Dennis <dennis.krijgsman@hotmail.com>
2021-02-01 15:30:23 +0100

    added url's

--- resources/js/components/Dashboard/DashboardWithHexagon.vue
+++ resources/js/components/Dashboard/DashboardWithHexagon.vue
@@ -1,32 +1,46 @@
 <template>
     <ul class="honeycomb" lang="es">
   <li class="honeycomb-cell">
-    <img class="honeycomb-cell__image" src="https://source.unsplash.com/random/1">
-    <div class="honeycomb-cell__title">Ons aanbod</div>
+    <a href="/activiteitenkalender">
+      <img class="honeycomb-cell__image" src="https://source.unsplash.com/random/1">
+      <div class="honeycomb-cell__title">Ons aanbod</div>
+    </a>
   </li>
   <li class="honeycomb-cell">
-    <img class="honeycomb-cell__image" src="https://source.unsplash.com/random/2">
-    <div class="honeycomb-cell__title">Uw aanbod</div>
+    <a href="/workshop">
+      <img class="honeycomb-cell__image" src="https://source.unsplash.com/random/2">
+      <div class="honeycomb-cell__title">Uw aanbod</div>
+    </a>
   </li>
   <li class="honeycomb-cell">
-    <img class="honeycomb-cell__image" src="https://source.unsplash.com/random/3">
-    <div class="honeycomb-cell__title">Persoonlijke begeleiding</div>
+    <a href="/one-on-one">
+      <img class="honeycomb-cell__image" src="https://source.unsplash.com/random/3">
+      <div class="honeycomb-cell__title">Persoonlijke begeleiding</div>
+    </a>
   </li>
   <li class="honeycomb-cell">
-    <img class="honeycomb-cell__image" src="https://source.unsplash.com/random/4">
-    <div class="honeycomb-cell__title">Gamification</div>
+    <a href="/covid-19">
+      <img class="honeycomb-cell__image" src="https://source.unsplash.com/random/4">
+      <div class="honeycomb-cell__title">Gamification</div>
+    </a>
   </li>
   <li class="honeycomb-cell">
-    <img class="honeycomb-cell__image" src="https://source.unsplash.com/random/5">
-    <div class="honeycomb-cell__title">Vragenlijsten</div>
+    <a href="/vragenlijsten">
+      <img class="honeycomb-cell__image" src="https://source.unsplash.com/random/5">
+      <div class="honeycomb-cell__title">Vragenlijsten</div>
+    </a>
   </li>
   <li class="honeycomb-cell">
-    <img class="honeycomb-cell__image" src="https://source.unsplash.com/random/6">
-    <div class="honeycomb-cell__title">Inspiratiemateriaal</div>
+    <a href="/articles">
+      <img class="honeycomb-cell__image" src="https://source.unsplash.com/random/6">
+      <div class="honeycomb-cell__title">Inspiratiemateriaal</div>
+    </a>
   </li>
   <li class="honeycomb-cell">
-    <img class="honeycomb-cell__image" src="https://source.unsplash.com/random/7">
-    <div class="honeycomb-cell__title">Uw acedemy</div>
+    <a href="/goodhabitz">
+      <img class="honeycomb-cell__image" src="https://source.unsplash.com/random/7">
+      <div class="honeycomb-cell__title">Uw acedemy</div>
+    </a>
   </li>
   <li class="honeycomb-cell honeycomb__placeholder"></li>
 </ul>
