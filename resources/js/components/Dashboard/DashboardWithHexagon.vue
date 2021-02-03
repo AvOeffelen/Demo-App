eb0ba0a4978d5bbb85450a5684e8aafac0e50d5a
Dennis <dennis.krijgsman@hotmail.com>
2021-02-03 14:51:23 +0100

    Added new dashboard example

---
+++ resources/js/components/Dashboard/DashboardWithHexagon.vue
@@ -0,0 +1,67 @@
+<template>
+    <ul class="honeycomb" lang="es">
+  <li class="honeycomb-cell">
+    <a href="/activiteitenkalender">
+      <img class="honeycomb-cell__image" src="https://source.unsplash.com/random/1">
+      <div class="honeycomb-cell__title">Ons aanbod</div>
+    </a>
+  </li>
+  <li class="honeycomb-cell">
+    <a href="/workshop">
+      <img class="honeycomb-cell__image" src="https://source.unsplash.com/random/2">
+      <div class="honeycomb-cell__title">Uw aanbod</div>
+    </a>
+  </li>
+  <li class="honeycomb-cell">
+    <a href="/one-on-one">
+      <img class="honeycomb-cell__image" src="https://source.unsplash.com/random/3">
+      <div class="honeycomb-cell__title">Persoonlijke begeleiding</div>
+    </a>
+  </li>
+  <li class="honeycomb-cell">
+    <a href="/covid-19">
+      <img class="honeycomb-cell__image" src="https://source.unsplash.com/random/4">
+      <div class="honeycomb-cell__title">Gamification</div>
+    </a>
+  </li>
+  <li class="honeycomb-cell">
+    <a href="/vragenlijsten">
+      <img class="honeycomb-cell__image" src="https://source.unsplash.com/random/5">
+      <div class="honeycomb-cell__title">Vragenlijsten</div>
+    </a>
+  </li>
+  <li class="honeycomb-cell">
+    <a href="/articles">
+      <img class="honeycomb-cell__image" src="https://source.unsplash.com/random/6">
+      <div class="honeycomb-cell__title">Inspiratiemateriaal</div>
+    </a>
+  </li>
+  <li class="honeycomb-cell">
+    <a href="/goodhabitz">
+      <img class="honeycomb-cell__image" src="https://source.unsplash.com/random/7">
+      <div class="honeycomb-cell__title">Uw acedemy</div>
+    </a>
+  </li>
+  <li class="honeycomb-cell honeycomb__placeholder"></li>
+</ul>
+
+</template>
+
+<script>
+export default {
+    name: "DashboardWithHexagon",
+    data() {
+        return {
+
+        };
+    },
+    created() {
+    },
+    methods: {
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
