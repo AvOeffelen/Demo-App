e2d75c18a5a14040eeaa842147ebac2938074353
Dennis <dennis.krijgsman@hotmail.com>
2021-02-06 20:29:51 +0100

    browser check

--- resources/views/browser/failure.blade.php
+++ resources/views/browser/failure.blade.php
@@ -3,46 +3,48 @@
     <div class="bg-body-light">
         <div class="content content-full">
             <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center">
-                <h1 class="flex-sm-fill font-size-h2 font-w400 mt-2 mb-0 mb-sm-2">Generatiemanagement</h1>
+                <h1 class="flex-sm-fill font-size-h2 font-w400 mt-2 mb-0 mb-sm-2">Browser Update</h1>
                 <nav class="flex-sm-00-auto ml-sm-3" aria-label="breadcrumb">
                     <ol class="breadcrumb">
-                        <li class="breadcrumb-item">KLANTSAMENVITAAL</li>
-                        <li class="breadcrumb-item active" aria-current="page">Generatiemanagement</li>
+                        <li class="breadcrumb-item">Browser Update</li>
+                        <li class="breadcrumb-item active" aria-current="page">Browser Update</li>
                     </ol>
                 </nav>
             </div>
         </div>
     </div>
-    <div class="content content-full text-center">
-        <div class="row">
-            <div class="col-md-12 col-lg-2 col-xl-2 col-12 col-sm-12 col-xs-12 cols-12">
-
-                <a href="javascript:history.back()" class="btn btn-primary">Terug</a>
-            </div>
-            <div class="col-md-12 col-lg-8 col-xl-8 col-12 col-sm-12 col-xs-12">
-                <h3>Uw browser is verouderd.</h3>
-            </div>
-            <div class="col-md-12 col-lg-2 col-xl-2 col-12 col-sm-12 col-xs-12 cols-12">
-            </div>
-        </div>
+    <div class="content content-boxed">
         <div class="row">
             <div class="col-md-12 col-lg-2 col-xl-2 col-12 col-sm-12 col-xs-12 cols-12">
             </div>
             <div class="col-md-12 col-lg-8 col-xl-8 col-12 col-sm-12 col-xs-12">
                 <alinea class="text-center">
-                    pas dit maar lekker aan.
+                    <h1 class="alert alert-danger" role="alert">Uw browser is verouderd!</h1>  
                 </alinea>
             </div>
             <div class="col-md-12 col-lg-2 col-xl-2 col-12 col-sm-12 col-xs-12 cols-12">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-2 col-lg-2 col-xl-2 col-2 col-sm-2 col-xs-2">
+            </div>
+            <div class="col-md-8 col-lg-8 col-xl-8 col-8 col-sm-8 col-xs-8">
+                <b>Om te profiteren van alle mogelijkheden in ons portaal, raden we je aan om altijd de nieuwste versie van een browser te gebruiken. Update daarom altijd je browser als er een nieuwe versie beschikbaar is. Of installeer een andere browser. Voorbeelden van bekende browsers zijn Google Chrome, Firefox en Opera</b>
+            </div>
+            <div class="col-md-2 col-lg-2 col-xl-2 col-2 col-sm-2 col-xs-2">
             </div>
         </div>
         <div class="row py-5">
             <div class="col-md-2 col-lg-2 col-xl-2 col-2 col-sm-2 col-xs-2">
             </div>
             <div class="col-md-8 col-lg-8 col-xl-8 col-8 col-sm-8 col-xs-8">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/B9h7yuJ9SW4" frameborder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowfullscreen></iframe>
+                <h2>Update je browser</h2>
+                <p>We raden je aan om altijd de nieuwste versie van een browser te gebruiken. Installeer daarom in een paar stappen de nieuwste versie van:</p>
+                <ul class="list-group">
+                    <li class="list-group-item"><a href="https://www.google.com/intl/nl_ALL/chrome/">Download Google Chrome</a></li>
+                    <li class="list-group-item"><a href="https://www.mozilla.org/nl/firefox/new/">Download Firefox</a></li>
+                    <li class="list-group-item"><a href="https://www.opera.com">Download Opera</a></li>
+                </ul>
             </div>
             <div class="col-md-2 col-lg-2 col-xl-2 col-2 col-sm-2 col-xs-2">
             </div>
