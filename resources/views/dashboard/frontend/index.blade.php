981a8889b1f261f826ff20f360267f37125889f6
Dennis <dennis.krijgsman@hotmail.com>
2021-02-10 15:36:01 +0100

    changed header logo

--- resources/views/dashboard/frontend/index.blade.php
+++ resources/views/dashboard/frontend/index.blade.php
@@ -5,10 +5,11 @@
     <div class="bg-body-white">
         <div class="content content-full">
             <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center bravis-logo">
-                <img src="storage/logo/logo.jpg" height="">
-            </div>
-            <div class="text-center">
-                <p>Deze Gelre Energiek Gids is uitsluitend bestemd voor medewerkers van Gelre ziekenhuizen.</p>
+                <img src="storage/logo/logo.jpg" height="70">
+                <div class="text-center justify-content-center" height="70">
+                    <p>Deze Gelre Energiek Gids is uitsluitend bestemd voor medewerkers van Gelre ziekenhuizen.</p>
+                </div>
+                <div class=""></div>
             </div>
         </div>
     </div>
