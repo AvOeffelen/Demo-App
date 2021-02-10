537afa949a25a28fc39600d243157158d6180579
Dennis <dennis.krijgsman@hotmail.com>
2021-02-10 15:51:07 +0100

    slide text

--- resources/views/dashboard/frontend/index.blade.php
+++ resources/views/dashboard/frontend/index.blade.php
@@ -5,11 +5,9 @@
     <div class="bg-body-white">
         <div class="content content-full">
             <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center bravis-logo">
-                <img src="storage/logo/logo.jpg">
-                <div class="text-center justify-content-center">
-                    <p></p>
-                </div>
-                <div class=""></div>
+                <img src="/storage/logo/logo.jpg">
+                <marquee width="100%" direction="left">Tip van de week: Blijf sporten. Ook al beweeg je op de werkvloer.</marquee>
+                <div></div>
             </div>
         </div>
     </div>
