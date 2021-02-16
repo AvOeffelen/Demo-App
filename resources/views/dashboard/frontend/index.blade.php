2992102fe593486955022cca821b4efb6f4eb0e9
Dennis <dennis.krijgsman@hotmail.com>
2021-02-17 00:32:53 +0100

    demo

--- resources/views/dashboard/frontend/index.blade.php
+++ resources/views/dashboard/frontend/index.blade.php
@@ -6,6 +6,10 @@
         <div class="content content-full">
             <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center">
                 <img src="/storage/logo/logo.png" height="">
+                <marquee width="60%" direction="left">Tip van de week: Blijf sporten. Ook al beweeg je op de werkvloer.</marquee>
+                <a href="/topical">
+                    <i class="fas fa-feather-alt "> Stories & Articles</i>
+                </a>
             </div>
         </div>
     </div>
