b1453436aa1d04839493653bdab3c8c41cfc6209
Dennis <dennis.krijgsman@hotmail.com>
2021-02-16 17:06:52 +0100

    demo

--- resources/js/components/Dashboard/DashboardWithHexagon.vue
+++ resources/js/components/Dashboard/DashboardWithHexagon.vue
@@ -1,45 +1,46 @@
 <template>
     <ul class="honeycomb" lang="es">
+      
   <li class="honeycomb-cell">
-    <a href="/activiteitenkalender">
-      <img class="honeycomb-cell__image" src="https://source.unsplash.com/random/1">
-      <div class="honeycomb-cell__title">Ons aanbod</div>
+    <a href="/goodhabitz">
+      <img class="honeycomb-cell__image" src="/storage/vakken/goodhabitz.png">
+      <div class="honeycomb-cell__title">GoodHabitz</div>
     </a>
   </li>
   <li class="honeycomb-cell">
     <a href="/workshop">
-      <img class="honeycomb-cell__image" src="https://source.unsplash.com/random/2">
-      <div class="honeycomb-cell__title">Uw aanbod</div>
+      <img class="honeycomb-cell__image" src="/storage/vakken/workshops.jpg">
+      <div class="honeycomb-cell__title">Workshops</div>
+    </a>
+  </li>
+  <li class="honeycomb-cell">
+    <a href="/workshop">
+      <img class="honeycomb-cell__image" src="/storage/vakken/health.jpg">
+      <div class="honeycomb-cell__title">Health</div>
+    </a>
+  </li>
+  <li class="honeycomb-cell">
+    <a href="/workshop">
+      <img class="honeycomb-cell__image" src="/storage/vakken/wellness.jpg">
+      <div class="honeycomb-cell__title">Wellness</div>
+    </a>
+  </li>
+   <li class="honeycomb-cell">
+    <a href="/articles">
+      <img class="honeycomb-cell__image" src="/storage/vakken/inspiration.jpg">
+      <div class="honeycomb-cell__title">Inspiration material</div>
+    </a>
+  </li>
+  <li class="honeycomb-cell">
+    <a href="/dashboard">
+      <img class="honeycomb-cell__image" src="/storage/vakken/analysis.jpg">
+      <div class="honeycomb-cell__title">Management information</div>
     </a>
   </li>
   <li class="honeycomb-cell">
     <a href="/one-on-one">
-      <img class="honeycomb-cell__image" src="https://source.unsplash.com/random/3">
-      <div class="honeycomb-cell__title">Persoonlijke begeleiding</div>
-    </a>
-  </li>
-  <li class="honeycomb-cell">
-    <a href="/covid-19">
-      <img class="honeycomb-cell__image" src="https://source.unsplash.com/random/4">
-      <div class="honeycomb-cell__title">Gamification</div>
-    </a>
-  </li>
-  <li class="honeycomb-cell">
-    <a href="/vragenlijsten">
-      <img class="honeycomb-cell__image" src="https://source.unsplash.com/random/5">
-      <div class="honeycomb-cell__title">Vragenlijsten</div>
-    </a>
-  </li>
-  <li class="honeycomb-cell">
-    <a href="/articles">
-      <img class="honeycomb-cell__image" src="https://source.unsplash.com/random/6">
-      <div class="honeycomb-cell__title">Inspiratiemateriaal</div>
-    </a>
-  </li>
-  <li class="honeycomb-cell">
-    <a href="/goodhabitz">
-      <img class="honeycomb-cell__image" src="https://source.unsplash.com/random/7">
-      <div class="honeycomb-cell__title">Uw acedemy</div>
+      <img class="honeycomb-cell__image" src="/storage/vakken/personal.jpg">
+      <div class="honeycomb-cell__title">Personal support</div>
     </a>
   </li>
   <li class="honeycomb-cell honeycomb__placeholder"></li>
--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -1,31 +1,4 @@
 @extends('layouts.user')
-
-<div class="content content-full">
-            <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center">
-                <h1 class="flex-sm-fill font-size-h2 font-w400 mt-2 mb-0 mb-sm-2">Profiel</h1>
-                <nav class="flex-sm-00-auto ml-sm-3" aria-label="breadcrumb">
-                    <ol class="breadcrumb">
-                        <li class="breadcrumb-item">KLANTSAMENVITAAL</li>
-                        <li class="breadcrumb-item active" aria-current="page">Profiel</li>
-                    </ol>
-                </nav>
-            </div>
-        </div>
-    </div>
-    <div class="content content-full text-center">
-        <div class="row">
-            <div class="col-md-12 col-lg-2 col-xl-2 col-12 col-sm-12 col-xs-12 cols-12">
-
-                <a href="javascript:history.back()" class="btn btn-primary">Back</a>
-            </div>
-            <div class="col-md-12 col-lg-8 col-xl-8 col-12 col-sm-12 col-xs-12">
-                <h3>Profiel</h3>
-            </div>
-            <div class="col-md-12 col-lg-2 col-xl-2 col-12 col-sm-12 col-xs-12 cols-12">
-            </div>
-        </div>
-    </div>
-
 @section('content')
     <div class="content content-full">
         <profile :user="{{auth()->user()}}"></profile>
