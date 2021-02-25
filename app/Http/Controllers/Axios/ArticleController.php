d6b64e26d012a6a5876cb7a766b9aef03d9362cc
Armando <armandovanoeffelen@gmail.com>
2021-02-25 13:47:54 +0100

    dit changed

--- app/Http/Controllers/Axios/ArticleController.php
+++ app/Http/Controllers/Axios/ArticleController.php
@@ -151,11 +151,14 @@ class ArticleController extends Controller
         }
     }
 
-    public function getAllArticles()
+    /**
+     * @return JsonResponse
+     */
+    public function getAllArticles():JsonResponse
     {
         $articles = Article::with('Category')->get();
 
-        return $articles;
+        return response()->json(['data' => $articles]);
     }
 
     public function getAllCategories()
