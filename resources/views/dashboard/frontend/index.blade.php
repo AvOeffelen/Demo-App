e10372e7d51ff3cde98db2352dead9c4df6c06e9
Dennis <dennis.krijgsman@hotmail.com>
2021-03-08 00:48:09 +0100

    changed tip

--- resources/views/dashboard/frontend/index.blade.php
+++ resources/views/dashboard/frontend/index.blade.php
@@ -6,7 +6,7 @@
         <div class="content content-full">
             <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center bravis-logo">
                 <img src="/storage/logo/logo.jpg">
-                <marquee width="100%" direction="left">Tip van de week: 1 maart complimentendag. Zeg iets positiefs over iemands gedrag of persoonlijkheid.</marquee>
+                <marquee width="100%" direction="left">Tip van de week: Vermijd koffie als je hoofdpijn hebt.</marquee>
                 <div></div>
             </div>
         </div>
