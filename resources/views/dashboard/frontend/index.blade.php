99396a478c31dfe2bd2e1722ecb09a04e23bdfee
Dennis <dennis.krijgsman@hotmail.com>
2021-03-15 00:45:57 +0100

    changed tip

--- resources/views/dashboard/frontend/index.blade.php
+++ resources/views/dashboard/frontend/index.blade.php
@@ -6,7 +6,7 @@
         <div class="content content-full">
             <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center bravis-logo">
                 <img src="/storage/logo/logo.jpg">
-                <marquee width="100%" direction="left">Tip van de week: Vermijd koffie als je hoofdpijn hebt.</marquee>
+                <marquee width="100%" direction="left">Tip voor thuiswerkers: neem vaak een korte pauze.</marquee>
                 <div></div>
             </div>
         </div>
