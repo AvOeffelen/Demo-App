be623bcf16c79958560360b0438c17074c5e1426
Dennis <info@krijgsmansolutions.nl>
2021-04-05 21:00:43 +0200

    changed tip

--- resources/views/dashboard/frontend/index.blade.php
+++ resources/views/dashboard/frontend/index.blade.php
@@ -6,7 +6,7 @@
         <div class="content content-full">
             <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center bravis-logo">
                 <img src="/storage/logo/logo.jpg" height="70">
-                <marquee width="100%" direction="left">Zorg voor mensen om je heen waarmee je kunt lachen of waarvan jij vindt dat ze humor hebben.</marquee>
+                <marquee width="100%" direction="left">Bouw je (wandel)conditie rustig op.</marquee>
             </div>
         </div>
     </div>
